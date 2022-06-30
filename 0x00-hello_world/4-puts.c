077af915766e3d036cf0adeaed374a60b6c5897c
franklinduru <duruobinnafranklin.com>
2022-06-30 15:53:46 -0700

    c-programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
 /* main - Entry point
- *
- * Return: Always 0 (sucess)
- *
-*/ int main(void){puts("\"Programming is like building a multilingual puzzle");return (0);}
+ * Return: Always 0 (success)
+*/ int main(void){puts("\"Programming is like building a multilingual puzzle"); return (0); }
