b7598acfb7a521570026ca03c1dfa7deba9070cb
franklinduru <duruobinnafranklin.com>
2022-06-30 15:09:22 -0700

    c-programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
 #!/bin/bash
-
+/
+ * main - Entry point
+ *  * Return: Always 0 (success)
+ *
+ *   */
+ int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
