144add04afe29785bd56bbd319b4578de7036c88
franklinduru <duruobinnafranklin.com>
2022-06-30 16:37:06 -0700

    fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-/* main - Entry point
+/*
+ * main - Entry point
  * Return: Always 0 (success)
  */ int main(void)
 {printf("with proper grammar, but the outcome is a piece of art,\n");
