d4446471fb4871d1c42f8c77f14d5f2331159455
franklinduru <duruobinnafranklin.com>
2022-06-30 15:03:27 -0700

    c-programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,16 @@
 #!/bin/bash
+n - Entry point
 
+ * Return: Always 0 (sucess)
+ *
+ *  */
+
+ int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
