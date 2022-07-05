3beb04b586d047b3d8c651769f371e890ac72e1b
franklinduru <duruobinnafranklin.com>
2022-07-05 11:59:59 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,15 +14,15 @@ void print_to_98(int n)
 
 while (n < 98)
 {
-		printf("%i, ", n);
-		n++;
+	printf("%i, ", n);
+	n++;
 }
 while (n > 98)
 {
-		printf("%i, ", n);
-		n--;
+	printf("%i, ", n);
+	n--;
 }
 
-	printf("98");
-	putchar('\n');
+printf("98");
+putchar('\n');
 }
