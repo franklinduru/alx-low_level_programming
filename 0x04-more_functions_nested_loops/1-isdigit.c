014c45d1e2220dcbef910b71ce6d73d957506d40
franklinduru <duruobinnafranklin.com>
2022-07-07 06:49:56 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 
 /**
  * _isdigit - checks for characters that are digits
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,7 +4,7 @@
  * @n:.input integer parameter
  *
  *
- *      */
+ */
 void print_number(int n)
 {
 	unsigned int i = n;
