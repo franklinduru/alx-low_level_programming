250d39d58058248a6c3b83c973ec446eb89a2c0d
franklinduru <duruobinnafranklin.com>
2022-07-14 10:00:22 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,18 +8,18 @@
  * Return: String
  */
 
-	char *string_toupper(char *p)
+char *string_toupper(char *p)
+{
+
+	int i;
+		
+	for (i = 0; p[i] != '\0'; i++)
 	{
 
-		int i;
-		
-		for (i = 0; p[i] != '\0'; i++)
+		if (p[i] >= 97 && p[i] <= 122)
 		{
-
-			if (p[i] >= 97 && p[i] <= 122)
-			{
-				p[i] = p[i] - 32;
-			}
+			p[i] = p[i] - 32;
+		}
 
 	}
 
